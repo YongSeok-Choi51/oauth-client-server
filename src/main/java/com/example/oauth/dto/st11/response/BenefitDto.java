c73ca0ffb7b0c22834c3bab4c6ffbca91837058d
Yongseok-Choi51 <dydtjr1717@pioncorp.com>
2023-11-29 13:07:01 +0900

    push gogo

--- src/main/java/com/example/oauth/dto/st11/response/BenefitDto.java
+++ src/main/java/com/example/oauth/dto/st11/response/BenefitDto.java
@@ -1,14 +1,12 @@
 package com.example.oauth.dto.st11.response;
 
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 public class BenefitDto {
     public int Discount;
